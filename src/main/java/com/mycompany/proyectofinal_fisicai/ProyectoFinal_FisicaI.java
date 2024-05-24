f0ed73220410b4b18958af187e4d9f4d5616f27b
Donovan-Alvarez <donaleg12@gmail.com>
2024-05-23 22:14:12 -0600

    first commit

---
+++ src/main/java/com/mycompany/proyectofinal_fisicai/ProyectoFinal_FisicaI.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectofinal_fisicai;
+
+/**
+ *
+ * @author donovanpc
+ */
+public class ProyectoFinal_FisicaI {
+
+    public static void main(String[] args) {
+        Panel_Inicial panel_Inicial = new Panel_Inicial();
+        panel_Inicial.setVisible(true);
+    }
+}
